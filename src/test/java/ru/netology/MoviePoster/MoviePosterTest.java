ee223b7e536144fdf4be226859f89737a4733c52
sonke <inwoker123@mail.ru>
2022-06-26 22:52:24 +0300

    New name in test and reformate code

--- src/test/java/ru/netology/MoviePoster/MoviePosterTest.java
+++ src/test/java/ru/netology/MoviePoster/MoviePosterTest.java
@@ -15,7 +15,7 @@ public class MoviePosterTest {
     String movie7 = "Номер Один";
 
     @Test
-    public void givesLimitLastMovies() {
+    public void shouldGiveLastMoviesLimit() {
         MoviePoster poster = new MoviePoster(3);
         poster.add(movie1);
         poster.add(movie2);
@@ -24,28 +24,28 @@ public class MoviePosterTest {
         poster.add(movie5);
         poster.add(movie6);
         poster.add(movie7);
-        String[] actual= poster.findLast();
-        String[] expected ={movie7,movie6,movie5};
-        Assertions.assertArrayEquals(expected,actual);
+        String[] actual = poster.findLast();
+        String[] expected = {movie7, movie6, movie5};
+        Assertions.assertArrayEquals(expected, actual);
     }
-    
+
     @Test
     public void limitMoreMovies() {
         MoviePoster poster = new MoviePoster(4);
         poster.add(movie1);
         poster.add(movie2);
         poster.add(movie3);
-        String[] actual= poster.findLast();
-        String[] expected ={movie3,movie2,movie1};
-        Assertions.assertArrayEquals(expected,actual);
+        String[] actual = poster.findLast();
+        String[] expected = {movie3, movie2, movie1};
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void emptyManager() {
+    public void absenceMoviesInManager() {
         MoviePoster poster = new MoviePoster(4);
-        String[] actual= poster.findLast();
-        String[] expected ={};
-        Assertions.assertArrayEquals(expected,actual);
+        String[] actual = poster.findLast();
+        String[] expected = {};
+        Assertions.assertArrayEquals(expected, actual);
     }
 }
 
